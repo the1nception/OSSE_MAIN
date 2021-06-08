7e74607af03462af5f4dbd8ceaa55cbf34fb8fe6
meyerjoh <meyerjoh@hs-furtwangen.de>
2021-06-08 12:57:38 +0200

    new Branch created (Aufgabe05)

---
+++ src/main/java/de/hfu/App.java
@@ -0,0 +1,28 @@
+package de.hfu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        try {
+            /**
+             * Javadoc Kommentar: Buffered Reader liest String ein
+             * @param eingabe ist der Buffered Reader
+             */
+            BufferedReader eingabe = new BufferedReader(new InputStreamReader(System.in));
+            String s = eingabe.readLine();
+            String caps = s.toUpperCase();
+            /**
+             * zweiter kommentar, nur so!
+             * @param caps ist der String mit Großbuchstaben
+             */
+            System.out.println(caps);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
